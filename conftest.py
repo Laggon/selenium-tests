bd960bc7793ef442129f01833ed8d464ad3ff6ee
Laggon <laggonroll@gmail.com>
2026-07-08 20:03:38 +0500

    Save screenshot if failed

--- conftest.py
+++ conftest.py
@@ -9,3 +9,12 @@ def driver():
     yield browser.driver
 
     browser.stop()
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get('driver')
+        driver.save_screenshot('latest.png')
