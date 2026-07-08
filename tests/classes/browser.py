636649170370c87ba1c978632a44d20197f5f922
Laggon <laggonroll@gmail.com>
2026-07-08 19:57:10 +0500

    Separate class Browser

---
+++ tests/classes/browser.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from classes.config import Config
+
+class Browser:
+    driver = None
+
+    def start(self):
+        options = webdriver.ChromeOptions()
+
+        if Config.headless():
+            options.add_argument("--headless")
+
+        options.add_experimental_option('prefs', {
+            'intl.accept_languages': 'ru',
+        })
+
+        service = ChromeService(ChromeDriverManager().install())
+
+        self.driver = webdriver.Chrome(options, service)
+
+        window_size = Config.window_size()
+
+        if len(window_size) == 2:
+            self.driver.set_window_size(window_size[0], window_size[1])
+        else:
+            self.driver.maximize_window()
+
+    def stop(self):
+        self.driver.quit()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,31 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 import pytest
-from classes.config import Config
+from classes.browser import Browser
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
+    browser = Browser()
+    browser.start()
 
-    if Config.headless():
-        options.add_argument("--headless")
+    yield browser.driver
 
-    options.add_experimental_option('prefs', {
-        'intl.accept_languages': 'ru',
-    })
-
-    service = ChromeService(ChromeDriverManager().install())
-
-    driver = webdriver.Chrome(options, service)
-
-    window_size = Config.window_size()
-
-    if len(window_size) == 2:
-        driver.set_window_size(window_size[0], window_size[1])
-    else:
-        driver.maximize_window()
-
-    yield driver
-
-    driver.quit()
+    browser.stop()
